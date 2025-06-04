9c5ead55f273eb46e03e4f1bbbf8e31bea504e5a
JayXie <tsum950210@gmail.com>
2025-06-04 21:58:00 +0800

    chore: Bump version to 1.1.0

--- ReviveUtils.cs
+++ ReviveUtils.cs
@@ -9,7 +9,7 @@ using ReviveUtils.Patches;
 
 namespace ReviveUtils
 {
-    [BepInPlugin("jaydev.ReviveUtils", "ReviveUtils", "1.0.0")]
+    [BepInPlugin("jaydev.ReviveUtils", "ReviveUtils", "1.1.0")]
     public class ReviveUtils : BaseUnityPlugin
     {
         internal static ReviveUtils Instance { get; private set; } = null!;
